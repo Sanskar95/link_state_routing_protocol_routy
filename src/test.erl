af463817d7a68fc58a1d120a6ab07145c404c007
Sanskar Gupta <Sanskar95@users.noreply.github.com>
2020-09-16 13:23:59 +0200

    added final router test.

--- src/test.erl
+++ src/test.erl
@@ -1,8 +1,8 @@
--module(test2).
--export([start_world/0, stop_world/0, connect_paris_berlin/0, start_triangle/0]).
+-module(test).
+-export([start/0, stop/0]).
 
 
-start_triangle()->
+start()->
   routy:start(a),
   io:format("[Test] Started router 'a'~n", []),
   routy:start(b),
@@ -15,15 +15,14 @@ start_triangle()->
   b ! {add, c, {c, node()}},
   io:format("[Test] Added 'b' to 'c'~n", []),
   c ! {add, a, {a, node()}},
-  io:format("[Test] Added 'b' to 'c'~n", []).
+  io:format("[Test] Added 'b' to 'c'~n", []),
+
+  broadcast_update([a, b, c]).
 
 broadcast_update(ProcessList)->
   lists:map(fun(Name) -> Name ! broadcast, timer:sleep(1000) end, ProcessList),
   lists:map(fun(Name) -> Name ! update, timer:sleep(1000) end, ProcessList).
 
 
-stop_world() ->
-  routy:stop(rome),
-  routy:stop(paris),
-  routy:stop(madrid),
-  routy:stop(berlin).
+stop() ->
+  lists:map(fun(Name) -> Name ! stop, timer:sleep(1000) end, [a,b,c]).
